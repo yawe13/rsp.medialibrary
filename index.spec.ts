114cc8433c0121517a85e71f15c58fa306c4755c
Bartsch, Patrick, Dr. (I/EE-511) <patrick.bartsch@audi.de>
2017-06-19 02:32:27 +0200

    initial commit

---
+++ index.spec.ts
@@ -0,0 +1,8 @@
+import * as Medialibrary from "./index";
+import { BehaviorSubject } from "@reactivex/rxjs";
+import { Element, ElementResponse, CollectionResponse} from "../rsiPlugin";
+
+
+describe("Service", () => {
+});
+
